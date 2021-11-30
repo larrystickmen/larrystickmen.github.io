5119eafc4157528d148ae63ca1371343f9e97ef9
kornrt2018 <79989518+kornrt2018@users.noreply.github.com>
2021-11-30 18:34:43 +0700

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,39 @@
+
+
+    function animate(obj, initVal, lastVal, duration) {
+
+        let startTime = null;
+
+        let currentTime = Date.now();
+
+        const step = (currentTime ) => {
+
+            if (!startTime) {
+            startTime = currentTime ;
+            }
+ 
+            const progress = Math.min((currentTime  - startTime) / duration, 1);
+          
+            obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }else{
+                window.cancelAnimationFrame(window.requestAnimationFrame(step));
+            }
+        };
+
+       
+        window.requestAnimationFrame(step);
+    }
+
+let text1 = document.getElementById('01');
+let text2 = document.getElementById('02');
+
+const load = () =>{
+
+    animate(text1, 0, 1702, 2000);
+    animate(text2, 0, 98, 2000);
+
+}
+
