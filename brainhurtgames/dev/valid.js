5f28cc815ea9f38f53f6c11f1788a66584092911
Korn <79989518+kornls@users.noreply.github.com>
2022-01-14 19:02:46 +0700

    Update valid.js

--- brainhurtgames/dev/valid.js
+++ brainhurtgames/dev/valid.js
@@ -1,7 +1,1 @@
-if !(document.getElementById("version").innerHTML == "0500" && document.getElementById("yearweek").innerHTML > 2202 && (document.getElementById("iden") % 7) == 0 && ( document.getElementById("type") == "DEV" || document.getElementById("type") == "PPT" ) ) {
-    alert("Invalid Developer Access Code. Please try again.")
-} else if (document.getElementById("type") == "DEV") {
-    window.location.replace("https://github.com/larrystudiosdevs/brainhurtgames/releases/download/v2.5.0-gamma.1/BrainHurtGames-v2.5.0-gamma.1.key")
-} else if (document.getElementById("type") == "PPT") {
-    window.location.replace("https://github.com/larrystudiosdevs/brainhurtgames/releases/download/v2.5.0-gamma.1/BrainHurtGames-v2.5.0-gamma.1.pptx")
-}
+
